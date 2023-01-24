95987fed18b747e9940940e7eb4efe407fbe8243
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-01-24 15:26:02 +0900

    add a dropout method on hyperparamtertuning.py
    add a dropout method to Sequential model
    In regards to functional model, please check 'https://hj0216.tistory.com/28'

--- AI/hyperParameter_Tuning.py
+++ AI/hyperParameter_Tuning.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
 
 
@@ -11,15 +11,22 @@ x = np.array([1,2,3,4,5])
 y = np.array([1,2,3,5,4])
 
 
-# 2. Model
+# 2. Model(Sequential)
 model = Sequential()
 model.add(Dense(64, input_dim=1))
 model.add(Dense(64)) # 이전 layer의 output이 다음 layer의 input이 되므로 생략 가능
+model.add(Dropout(0.3))
 model.add(Dense(32))
 model.add(Dense(16))
+model.add(Dropout(0.2))
 model.add(Dense(8))
 model.add(Dense(1))
-
+model.summary()
+'''
+summray
+node를 random하게 추출하여 훈련을 수행 -> 과적합 문제 보완
+summary는 dropout된 node를 나누지 않음 -> # param에 영향 X
+'''
 
 # 3. Complile, training에서의 HyperParameter_Tuning: optimizer, epochs, batch_size
 model.compile(loss='mae', optimizer='adam')
@@ -34,7 +41,7 @@ print("6의 결과: ", result)
 
 
 # Hyper-parameter tuning
-# Layer, Node
+# Layer, Node, Dropout
 # Optimizer
 # Epochs
 # Batch_size
