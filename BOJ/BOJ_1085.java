9797d5bc7d8ca0fc84b44583ea72d622991fb7d6
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-03-01 08:11:11 +0900

    Create BOJ_1085.java

---
+++ BOJ/BOJ_1085.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		
+		if(2*a>=x) {
+			if(2*b>=y) {
+				if(x-a>=y-b) {System.out.println(y-b);}
+				else {System.out.println(x-a);}
+			} else {
+				if(x-a>=b) {System.out.println(b);}
+				else {System.out.println(x-a);}				
+			}
+		} else {
+			if(2*b>=y) {
+				if(a>=y-b) {System.out.println(y-b);}
+				else {System.out.println(a);}
+			} else {
+				if(a>=b) {System.out.println(b);}
+				else {System.out.println(a);}
+			}
+		}
+		
+		scan.close();
+	}
+}
