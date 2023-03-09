32491cdabee7799c1612436ab4f694d6406edaa0
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-03-10 02:04:26 +0900

    Create BOJ_10869.java

---
+++ BOJ/BOJ_10869.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int a1 = scan.nextInt();
+		int a2 = scan.nextInt();
+		
+		System.out.println(a1 + a2);
+		System.out.println(a1 - a2);
+		System.out.println(a1 * a2);
+		System.out.println(a1 / a2);
+		System.out.println(a1 % a2);
+		
+	}
+}
