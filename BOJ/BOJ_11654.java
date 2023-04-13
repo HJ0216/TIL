7c109d7a547e73293482b45828e1b6da7ce8de40
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-04-13 20:18:16 +0900

    Create BOJ_11654.java

---
+++ BOJ/BOJ_11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		char a = s.charAt(0);
+		int i = a;
+		
+		System.out.println(i);
+	}
+}
