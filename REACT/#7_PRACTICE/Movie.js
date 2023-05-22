ec5289c9ad86422d8ae26c67e7bf980476fc2f12
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-05-22 23:24:24 +0900

    Update Movie with css.js

--- REACT/#7_PRACTICE/Movie.js
+++ REACT/#7_PRACTICE/Movie.js
@@ -4,19 +4,23 @@ import { Link } from "react-router-dom";
 import styles from "./Movie.module.css";
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
+
   return (
     <div>
-      <img src={coverImg} alt={title} />
-      <div>
-        <h2>
-          <Link to={`/movie/${id}`}>{title}</Link>
+      <img className={styles.movieImg} src={coverImg} alt={title} />
+      <div className={styles.movieContent}>
+        <div className={styles.movieTitle}>
+          <Link className={styles.movieTitleLink} to={`/movie/${id}`}>{title}</Link>
           {/* ${}: ``안에서 변수 선언 */}
-        </h2>
-        <h3 className={styles.movie__year}>{year}</h3>
-        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul>
-          {genres.map((g, index) => (
-            <li key={index}>{g}</li>
+        </div>
+        <div className={styles.movieYear}>{year}</div>
+        <br/>
+        <p className={styles.movieSummaryTitle}>Summary</p>
+        <p className={styles.movieSummary}>{summary.length > 80 ? `${summary.slice(0, 80)}...` : summary}</p>
+        <br/>
+        <ul className={styles.movieGenresUl}>
+          {genres.slice(0, 2).map((g, index) => (
+            <li className={styles.movieGenresLi} key={index}>#{g}</li>
           ))}
         </ul>
       </div>
