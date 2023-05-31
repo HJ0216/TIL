2dab85e475dfa805eceab8fbc623a6bccd478c06
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-06-01 08:23:33 +0900

    Create BOJ_15894.java

---
+++ BOJ/BOJ_15894.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		long n = Long.parseLong(br.readLine());
+
+		bw.write((4*n) + "");
+		bw.flush();
+		bw.close();
+
+	}
+}
