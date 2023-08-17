627a9f632803273d1996370cf26f9fcb0bdcf4d8
Hyunji <6120hj@gmail.com>
2023-08-17 16:35:31 +0900

    Doc: remove the unused libs on  BOJ_1037.java

--- BOJ/BOJ_1037.java
+++ BOJ/BOJ_1037.java
@@ -3,38 +3,31 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
- 
-        int min = 1_000_000;
-        int max = 1;
-        
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            int M = Integer.parseInt(st.nextToken());
-            
-            min = min <= M ? min : M;
-            max = max >= M ? max : M;
-            
-        }
- 
-        bw.write(min * max + "");
-        
-        bw.flush();
-        bw.close();
-    }
- 
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int min = 1_000_000;
+		int max = 1;
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int M = Integer.parseInt(st.nextToken());
+
+			min = min <= M ? min : M;
+			max = max >= M ? max : M;
+
+		}
+
+		bw.write(min * max + "");
+
+		bw.flush();
+		bw.close();
+	}
+
 }
